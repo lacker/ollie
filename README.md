2908eb2ff48b8d42457dfad539ad1545abac0e4f
Kevin Lacker <lacker@gmail.com>
2015-12-02 20:49:38 -0800

    finds ollies

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-To use this, first you need to figure out the id of your Ollie.
+First, enable bluetooth on your computer.
 
-* Enable bluetooth on a Macbook.
-* Run `cylon-ble-scan` (npm install -g it, and run with sudo, if needed)
-* Figure out which entry is the Ollie (might be name = 2B-12F4)
-* Set the UUID accordingly in config.json
+Then, run `npm start`
--- main.js
+++ main.js
@@ -1,6 +1,26 @@
 'use strict';
 
-let Cylon = require('cylon');
+let noble = require('noble');
 let config = require('./config.json');
-console.log('connecting to', config.uuid);
 
+noble.on('stateChange', (state) => {
+  console.log('noble state =', state);
+  if (state != 'poweredOn') {
+    return;
+  }
+  
+  noble.startScanning();
+});
+
+noble.on('discover', (peripheral) => {
+  let localName = peripheral.advertisement.localName;
+  console.log('discovered', localName, peripheral.id);
+
+  if (localName !== '2B-12F4') {
+    // Not an Ollie
+    console.log('ignoring the', localName);
+    return;
+  }
+
+  
+});
